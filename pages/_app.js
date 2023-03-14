2db4cc8d28737dc39c5578b9e58759cb5dda91e4
Triicky <mrtriickyx@gmail.com>
2023-03-14 21:20:52 +0100

    feat: skin

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/normalize.css'
+import '@/styles/globals.scss'
 import 'regenerator-runtime/runtime'
 import { Web3ReactProvider } from '@web3-react/core'
 import Web3 from 'web3'
+import localFont from 'next/font/local'
+
+const Gilroy = localFont({
+  src: [
+    { path: './Gilroy-ExtraBold.woff2', weight: '700', style: 'normal' },
+    { path: './Gilroy-Light.woff2', weight: '400', style: 'normal' },
+  ]
+})
 
 function getLibrary(provider) {
   return new Web3(provider)
@@ -10,7 +19,9 @@ function getLibrary(provider) {
 export default function App({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
+      <div className={Gilroy.className}>
+        <Component {...pageProps} />
+      </div>
     </Web3ReactProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,104 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Dictaphone from '@/components/Dictaphone'
-import { useWeb3React } from "@web3-react/core"
-import { injected, walletconnect } from "../components/wallet/Connectors"
-import { useState } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Dictaphone from "@/components/Dictaphone";
+import { useWeb3React } from "@web3-react/core";
+import { injected, walletconnect } from "../components/wallet/Connectors";
+import { useRef, useState } from "react";
+import Logo from "../public/logo.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
 
-  const [chatResponse, setChatResponse] = useState()
+  const inputRef = useRef();
+  const [chatResponses, setChatResponses] = useState([]);
+  const [userRequest, setUserRequest] = useState();
 
   async function connect(connector) {
     try {
-      await activate(connector === 'metamask' ? injected : walletconnect)
+      await activate(connector === "metamask" ? injected : walletconnect);
     } catch (ex) {
-      console.log(ex)
+      console.log(ex);
     }
   }
 
-  async function disconnect() {
-    try {
-      deactivate()
-    } catch (ex) {
-      console.log(ex)
-    }
-  }
+  async function callApi(evt) {
+    evt.preventDefault();
 
-  async function callApi() {
-    const res = await fetch('https://api-hosted.graphlinq.io/d0f19a36cc3bd4f60fe21bdd4f69879d6b11ce7b2381c44ee0c29cc71a3561e8/chat?chat_id=' + account, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    setChatResponses((current) => [
+      ...current,
+      {
+        who: "user",
+        value: userRequest,
       },
-      body: JSON.stringify({
-        prompt: "What is graphlinq protocol ?"
-      })
-    }).then((res) => res.json());
+    ]);
 
-    setChatResponse(res.response);
-  }
+    setUserRequest('');
+    inputRef.current.value = '';
+
+    window.setTimeout(() => {
+      setChatResponses((current) => [
+        ...current,
+        {
+          who: "bot",
+          value: "is typing ...",
+        },
+      ]);
+    }, 500);
+
+    try {
+      const res = await fetch(
+        "https://api-hosted.graphlinq.io/d0f19a36cc3bd4f60fe21bdd4f69879d6b11ce7b2381c44ee0c29cc71a3561e8/chat?chat_id=" +
+          account,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: userRequest,
+          }),
+        }
+      ).then((res) => res.json());
   
+      setChatResponses((current) => {
+        const responses = [...current];
+        responses.pop();
+
+        return [
+          ...responses,
+          {
+            who: "bot",
+            value: res.response,
+          },
+        ]
+      });
+    } catch (error) {
+
+      setChatResponses((current) => {
+        const responses = [...current];
+        responses.pop();
+
+        return [
+          ...responses,
+          {
+            who: "bot",
+            value: "I am currently not available, please try again later.",
+          },
+        ]
+      });
+    }
+
+
+  }
+
+  async function handleChange(evt) {
+    setUserRequest(evt.target.value);
+  }
+
   return (
     <>
       <Head>
@@ -51,22 +106,80 @@ export default function Home() {
         <meta name="description" content="GraphLinq Chat" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <div>
-        <h1>GraphLinq Chat</h1>
+      <div className="glq-home">
+        <Image src={Logo} className="glq-home-logo" />
 
-        <Dictaphone/>
+        {active ? (
+          <div className="glq-home-chat">
+            <div className="glq-home-chat-list">
+              {[...chatResponses].reverse().map((response) => (
+                <div
+                  className="glq-home-chat-response"
+                  data-who={response.who}
+                  key={response.value}
+                >
+                  <div className="glq-home-chat-response-who">
+                    {response.who === "bot" ? (
+                      <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><g><rect fill="none" height="24" width="24" y="0"/></g><g><g><path d="M20,9V7c0-1.1-0.9-2-2-2h-3c0-1.66-1.34-3-3-3S9,3.34,9,5H6C4.9,5,4,5.9,4,7v2c-1.66,0-3,1.34-3,3s1.34,3,3,3v4 c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-4c1.66,0,3-1.34,3-3S21.66,9,20,9z M18,19L6,19V7h12V19z M9,13c-0.83,0-1.5-0.67-1.5-1.5 S8.17,10,9,10s1.5,0.67,1.5,1.5S9.83,13,9,13z M16.5,11.5c0,0.83-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S14.17,10,15,10 S16.5,10.67,16.5,11.5z M8,15h8v2H8V15z"/></g></g></svg>
+                    ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"/></svg>
+                    )}
+                  </div>
+                  <div className="glq-home-chat-response-value">
+                    {response.value}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <form className="glq-home-chat-actions" onSubmit={callApi}>
+              <input
+                type="text"
+                placeholder="What's your question ?"
+                onChange={handleChange}
+                ref={inputRef}
+              />
+              <button type="submit" className="bt">
+                Ask me
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="glq-home-intro">
+            <div className="glq-home-title dg">
+              Welcome to <span>GraphLinq Chat</span>
+            </div>
+            <div className="glq-home-desc">
+              Alex is the public library of the internet, powered by OpenAI
+              models. Ask any question, and it will find an answer. Give it a
+              task, and it will deliver. Embrace the AI revolution.
+            </div>
 
-        <div>
-          <button onClick={() => connect('metamask')}>Connect to MetaMask</button>
-          <button onClick={() => connect('walletconnect')}>Connect to WalletConnect</button>
-          {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
-          <button onClick={disconnect}>Disconnect</button>
-        </div>
+            <div className="glq-home-connect">
+              <button
+                className="bt metamask"
+                onClick={() => connect("metamask")}
+              >
+                Connect to MetaMask
+              </button>
+              <button
+                className="bt walletconnect"
+                onClick={() => connect("walletconnect")}
+              >
+                Connect to WalletConnect
+              </button>
+            </div>
+          </div>
+        )}
 
-        {active ? <button onClick={callApi}>callApi</button> : null}
-        {chatResponse}
+        {/* <Dictaphone/> */}
       </div>
     </>
-  )
+  );
 }
