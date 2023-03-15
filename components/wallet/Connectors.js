71ab34a7744c2ffe1eb9c53e022096f0b2215e81
Triicky <mrtriickyx@gmail.com>
2023-03-15 10:12:06 +0100

    feat: add glqchainid to login

--- components/wallet/Connectors.js
+++ components/wallet/Connectors.js
@@ -2,10 +2,10 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1],
+  supportedChainIds: [1, 614],
 })
 
 
 export const walletconnect = new WalletConnectConnector({
-  supportedChainIds: [1],
+  supportedChainIds: [1, 614],
 })
